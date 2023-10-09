4788e57dbb56c22d67726b08b394fc1a2a34cf4d
Kseniia313 <kshome@mail.ru>
2023-10-09 12:03:49 +0300

    Первый коммит

---
+++ src/main/java/org/example/BubbleSort.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static int[] generateRandomArray() {
+
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[100_000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    private static void swapElements(int[] arr, int indexA, int indexB) {
+        int tmp = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = tmp;
+    }
+
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swapElements(arr, j, j + 1);
+
+                }
+            }
+        }
+    }
+
+}
